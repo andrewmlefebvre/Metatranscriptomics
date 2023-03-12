cc8fc9b28ad67c0da72e9201f81b99d7e802e1ca
andrewmlefebvre <andrewmlefebvre@gmail.com>
2023-03-11 21:00:21 -0500

    preprocessing work

--- Main.py
+++ Main.py
@@ -2,6 +2,6 @@ import os
 
 if __name__ == '__main__':
     # Delete all previous outputs
-    os.system("snakemake  --snakefile rules/Snakefile --delete-all-output")
+    os.system("snakemake  --snakefile rules/Snakefile --delete-all-output --cores 1")
     # Run from start
-    os.system("snakemake  --snakefile rules/Snakefile")
+    os.system("snakemake  --snakefile rules/Snakefile --cores 1")
--- rules/01-fastqc.smk
+++ rules/01-fastqc.smk
@@ -8,7 +8,6 @@ rule fastqc:
         touch("flags/fastqc.done")
     shell:
         '''
-        echo Here
         rm -r out/* 
         fastqc data/SRR000676.fastq.gz -o out
         '''
